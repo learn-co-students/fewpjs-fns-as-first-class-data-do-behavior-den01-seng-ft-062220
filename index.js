beb09ebf8312af11381b51afc6e36704aef6b8ca
Derek Turner <turner.derek.b@gmail.com>
2020-08-07 12:53:49 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+const $greeting = document.querySelector('h1')
+const morning = "Good Morning"
+const afternoon = "Good Afternoon"
+const evening = "Good Evening"
 
+function greet(time) {
+  let sTime = time.split(":")
+  let iTime = parseInt(sTime)
+    if (iTime < 12) {
+    return morning
+  } else if (iTime > 11 && iTime < 18) {
+    return afternoon
+  } else {
+    return evening
+  }
+}
+
+
+function displayMessage(greet) {
+  greeting.innerText = greet
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
